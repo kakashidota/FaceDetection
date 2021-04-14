9aba96c3e079d8dc4b6ef9e754673759f174b690
Kakashidota <robin.kamo@live.se>
2021-04-14 22:18:52 +0200

    Functioning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const URLAPI = `http://localhost:5000`
+
 function App() {
+
+  const [data, setData] = useState([])
+  // eslint-disable-next-line
+  const [image, setImage] = useState('https://www.kienyke.com/wp-content/uploads/2018/10/selfie.jpg')
+
+  const handleOnChange = event => {
+    setImage(event.target.value)
+  }
+
+  const handleClickImage = async event => {
+    event.preventDefault()
+    console.log('click')
+    try {
+      const fetchOptions = {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          image: image,
+        })
+      }
+
+      const resp = await fetch(`${URLAPI}/create-facelist`, fetchOptions)
+      const people = await resp.json()
+      console.log(people.data)
+      setData(people.data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,13 +47,48 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Upload a JPG image
+        </p>
+        <div className="containerFile">
+          <input
+            className="inputFile"
+            placeholder="Upload image"
+            onChange={handleOnChange}
+            value={image}
+          />
+          <button
+            className="buttonFile"
+            onClick={handleClickImage}
+          >
+            Upload
+          </button>
+        </div>
+        <h3 className="titleAtribute">Image attributes: </h3>
+        <ul>
+        {
+          data.map(item => (
+            <li key={item.faceId}>
+              <span>
+                Gender: {item.faceAttributes.gender}, age: {item.faceAttributes.age}
+              </span>
+            </li>
+          ))
+        }
+        </ul>
+        <img
+          src={image}
+          width={220}
+          height={180}
+          alt={image}
+        />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={image}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link of the image: {image}
         </a>
       </header>
     </div>
